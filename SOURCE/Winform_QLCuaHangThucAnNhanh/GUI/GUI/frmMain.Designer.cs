71e29ed6d6cc907435e225c7370ba6defb3c811f
Duc-Thien-IT <ducthien0912.dev@gmail.com>
2024-11-07 22:10:20 +0700

    Update GUI

--- SOURCE/Winform_QLCuaHangThucAnNhanh/GUI/GUI/frmMain.Designer.cs
+++ SOURCE/Winform_QLCuaHangThucAnNhanh/GUI/GUI/frmMain.Designer.cs
@@ -29,43 +29,122 @@
 		private void InitializeComponent()
 		{
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
-			this.panel1 = new System.Windows.Forms.Panel();
-			this.panel2 = new System.Windows.Forms.Panel();
-			this.pictureBox1 = new System.Windows.Forms.PictureBox();
-			this.label1 = new System.Windows.Forms.Label();
-			this.button1 = new System.Windows.Forms.Button();
+			this.pnlMenu = new System.Windows.Forms.Panel();
+			this.btnThongke = new System.Windows.Forms.Button();
+			this.btnDatmon = new System.Windows.Forms.Button();
+			this.btnQlddh = new System.Windows.Forms.Button();
+			this.btnQlma = new System.Windows.Forms.Button();
 			this.btnDangxuat = new System.Windows.Forms.Button();
-			this.button2 = new System.Windows.Forms.Button();
-			this.button3 = new System.Windows.Forms.Button();
-			this.button4 = new System.Windows.Forms.Button();
-			this.button5 = new System.Windows.Forms.Button();
-			this.panel1.SuspendLayout();
+			this.btnQlnd = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.pnlContent = new System.Windows.Forms.Panel();
+			this.pnlMenu.SuspendLayout();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
 			this.SuspendLayout();
 			// 
-			// panel1
+			// pnlMenu
 			// 
-			this.panel1.BackColor = System.Drawing.Color.Red;
-			this.panel1.Controls.Add(this.button5);
-			this.panel1.Controls.Add(this.button4);
-			this.panel1.Controls.Add(this.button3);
-			this.panel1.Controls.Add(this.button2);
-			this.panel1.Controls.Add(this.btnDangxuat);
-			this.panel1.Controls.Add(this.button1);
-			this.panel1.Controls.Add(this.label1);
-			this.panel1.Controls.Add(this.pictureBox1);
-			this.panel1.Location = new System.Drawing.Point(1, 2);
-			this.panel1.Name = "panel1";
-			this.panel1.Size = new System.Drawing.Size(265, 758);
-			this.panel1.TabIndex = 0;
+			this.pnlMenu.BackColor = System.Drawing.Color.Red;
+			this.pnlMenu.Controls.Add(this.btnThongke);
+			this.pnlMenu.Controls.Add(this.btnDatmon);
+			this.pnlMenu.Controls.Add(this.btnQlddh);
+			this.pnlMenu.Controls.Add(this.btnQlma);
+			this.pnlMenu.Controls.Add(this.btnDangxuat);
+			this.pnlMenu.Controls.Add(this.btnQlnd);
+			this.pnlMenu.Controls.Add(this.label1);
+			this.pnlMenu.Controls.Add(this.pictureBox1);
+			this.pnlMenu.Location = new System.Drawing.Point(1, 2);
+			this.pnlMenu.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.pnlMenu.Name = "pnlMenu";
+			this.pnlMenu.Size = new System.Drawing.Size(353, 933);
+			this.pnlMenu.TabIndex = 0;
 			// 
-			// panel2
+			// btnThongke
 			// 
-			this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-			this.panel2.Location = new System.Drawing.Point(272, 2);
-			this.panel2.Name = "panel2";
-			this.panel2.Size = new System.Drawing.Size(1031, 758);
-			this.panel2.TabIndex = 1;
+			this.btnThongke.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.btnThongke.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnThongke.Location = new System.Drawing.Point(52, 507);
+			this.btnThongke.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.btnThongke.Name = "btnThongke";
+			this.btnThongke.Size = new System.Drawing.Size(249, 53);
+			this.btnThongke.TabIndex = 11;
+			this.btnThongke.Text = "Thống kê báo cáo";
+			this.btnThongke.UseVisualStyleBackColor = true;
+			// 
+			// btnDatmon
+			// 
+			this.btnDatmon.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.btnDatmon.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnDatmon.Location = new System.Drawing.Point(52, 427);
+			this.btnDatmon.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.btnDatmon.Name = "btnDatmon";
+			this.btnDatmon.Size = new System.Drawing.Size(249, 53);
+			this.btnDatmon.TabIndex = 10;
+			this.btnDatmon.Text = "Đặt món";
+			this.btnDatmon.UseVisualStyleBackColor = true;
+			// 
+			// btnQlddh
+			// 
+			this.btnQlddh.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.btnQlddh.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnQlddh.Location = new System.Drawing.Point(52, 342);
+			this.btnQlddh.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.btnQlddh.Name = "btnQlddh";
+			this.btnQlddh.Size = new System.Drawing.Size(249, 53);
+			this.btnQlddh.TabIndex = 9;
+			this.btnQlddh.Text = "Quản lý đơn đặt hàng";
+			this.btnQlddh.UseVisualStyleBackColor = true;
+			// 
+			// btnQlma
+			// 
+			this.btnQlma.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.btnQlma.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnQlma.Location = new System.Drawing.Point(52, 254);
+			this.btnQlma.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.btnQlma.Name = "btnQlma";
+			this.btnQlma.Size = new System.Drawing.Size(249, 53);
+			this.btnQlma.TabIndex = 8;
+			this.btnQlma.Text = "Quản lý món ăn";
+			this.btnQlma.UseVisualStyleBackColor = true;
+			// 
+			// btnDangxuat
+			// 
+			this.btnDangxuat.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.btnDangxuat.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnDangxuat.Location = new System.Drawing.Point(52, 866);
+			this.btnDangxuat.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.btnDangxuat.Name = "btnDangxuat";
+			this.btnDangxuat.Size = new System.Drawing.Size(249, 53);
+			this.btnDangxuat.TabIndex = 7;
+			this.btnDangxuat.Text = "Đăng xuất";
+			this.btnDangxuat.UseVisualStyleBackColor = true;
+			// 
+			// btnQlnd
+			// 
+			this.btnQlnd.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.btnQlnd.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnQlnd.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			this.btnQlnd.Location = new System.Drawing.Point(52, 172);
+			this.btnQlnd.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.btnQlnd.Name = "btnQlnd";
+			this.btnQlnd.RightToLeft = System.Windows.Forms.RightToLeft.No;
+			this.btnQlnd.Size = new System.Drawing.Size(249, 53);
+			this.btnQlnd.TabIndex = 2;
+			this.btnQlnd.Text = "Quản lý người dùng";
+			this.btnQlnd.UseVisualStyleBackColor = true;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.ForeColor = System.Drawing.SystemColors.Control;
+			this.label1.Location = new System.Drawing.Point(132, 105);
+			this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(68, 18);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Admin 1";
 			// 
 			// pictureBox1
 			// 
@@ -73,95 +152,35 @@
 			this.pictureBox1.ErrorImage = null;
 			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
 			this.pictureBox1.InitialImage = null;
-			this.pictureBox1.Location = new System.Drawing.Point(96, 10);
+			this.pictureBox1.Location = new System.Drawing.Point(128, 12);
+			this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
 			this.pictureBox1.Name = "pictureBox1";
-			this.pictureBox1.Size = new System.Drawing.Size(65, 70);
+			this.pictureBox1.Size = new System.Drawing.Size(87, 86);
 			this.pictureBox1.TabIndex = 0;
 			this.pictureBox1.TabStop = false;
 			// 
-			// label1
+			// pnlContent
 			// 
-			this.label1.AutoSize = true;
-			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label1.ForeColor = System.Drawing.SystemColors.Control;
-			this.label1.Location = new System.Drawing.Point(99, 85);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(59, 15);
-			this.label1.TabIndex = 1;
-			this.label1.Text = "Admin 1";
-			// 
-			// button1
-			// 
-			this.button1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.button1.Location = new System.Drawing.Point(39, 140);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(187, 43);
-			this.button1.TabIndex = 2;
-			this.button1.Text = "Quản lý người dùng";
-			this.button1.UseVisualStyleBackColor = true;
-			// 
-			// btnDangxuat
-			// 
-			this.btnDangxuat.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.btnDangxuat.Location = new System.Drawing.Point(39, 704);
-			this.btnDangxuat.Name = "btnDangxuat";
-			this.btnDangxuat.Size = new System.Drawing.Size(187, 43);
-			this.btnDangxuat.TabIndex = 7;
-			this.btnDangxuat.Text = "Đăng xuất";
-			this.btnDangxuat.UseVisualStyleBackColor = true;
-			// 
-			// button2
-			// 
-			this.button2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.button2.Location = new System.Drawing.Point(39, 206);
-			this.button2.Name = "button2";
-			this.button2.Size = new System.Drawing.Size(187, 43);
-			this.button2.TabIndex = 8;
-			this.button2.Text = "Quản lý món ăn";
-			this.button2.UseVisualStyleBackColor = true;
-			// 
-			// button3
-			// 
-			this.button3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.button3.Location = new System.Drawing.Point(39, 278);
-			this.button3.Name = "button3";
-			this.button3.Size = new System.Drawing.Size(187, 43);
-			this.button3.TabIndex = 9;
-			this.button3.Text = "Quản lý đơn đặt hàng";
-			this.button3.UseVisualStyleBackColor = true;
-			// 
-			// button4
-			// 
-			this.button4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.button4.Location = new System.Drawing.Point(39, 347);
-			this.button4.Name = "button4";
-			this.button4.Size = new System.Drawing.Size(187, 43);
-			this.button4.TabIndex = 10;
-			this.button4.Text = "Đặt món";
-			this.button4.UseVisualStyleBackColor = true;
-			// 
-			// button5
-			// 
-			this.button5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.button5.Location = new System.Drawing.Point(39, 412);
-			this.button5.Name = "button5";
-			this.button5.Size = new System.Drawing.Size(187, 43);
-			this.button5.TabIndex = 11;
-			this.button5.Text = "Thống kê báo cáo";
-			this.button5.UseVisualStyleBackColor = true;
+			this.pnlContent.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+			this.pnlContent.Location = new System.Drawing.Point(363, 2);
+			this.pnlContent.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.pnlContent.Name = "pnlContent";
+			this.pnlContent.Size = new System.Drawing.Size(1375, 933);
+			this.pnlContent.TabIndex = 1;
 			// 
 			// frmMain
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(1234, 761);
-			this.Controls.Add(this.panel2);
-			this.Controls.Add(this.panel1);
+			this.ClientSize = new System.Drawing.Size(1645, 937);
+			this.Controls.Add(this.pnlContent);
+			this.Controls.Add(this.pnlMenu);
+			this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
 			this.Name = "frmMain";
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
 			this.Text = "frmMain";
-			this.panel1.ResumeLayout(false);
-			this.panel1.PerformLayout();
+			this.pnlMenu.ResumeLayout(false);
+			this.pnlMenu.PerformLayout();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
 			this.ResumeLayout(false);
 
@@ -169,15 +188,15 @@
 
 		#endregion
 
-		private System.Windows.Forms.Panel panel1;
-		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.Panel pnlMenu;
+		private System.Windows.Forms.Panel pnlContent;
 		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.PictureBox pictureBox1;
 		private System.Windows.Forms.Button btnDangxuat;
-		private System.Windows.Forms.Button button1;
-		private System.Windows.Forms.Button button5;
-		private System.Windows.Forms.Button button4;
-		private System.Windows.Forms.Button button3;
-		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button btnQlnd;
+		private System.Windows.Forms.Button btnThongke;
+		private System.Windows.Forms.Button btnDatmon;
+		private System.Windows.Forms.Button btnQlddh;
+		private System.Windows.Forms.Button btnQlma;
 	}
 }
